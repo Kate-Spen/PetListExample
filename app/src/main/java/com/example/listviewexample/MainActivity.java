3db43ab6f9615f3a940a74c826be9c4ab5b3922f
Kate Spencer <katemspencer529@gmail.com>
2025-09-16 12:15:02 -0500

    coding in class

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -2,17 +2,27 @@ package com.example.listviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Random;
+
+import model.Pet;
 
 public class MainActivity extends AppCompatActivity {
-
+ListView petListWidget;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int selection = 1;
+        petListWidget = findViewById(R.id.petList);
+        Random rand = new Random();
+        int selection = 2;
         populateListView(selection);
+
     }
 
     private void populateListView(int selection) {
@@ -32,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
      * the content of a strings array resource
      */
     private void useStringResource() {
+        String [] contents = getResources().getStringArray(R.array.breeds);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, contents);
+        petListWidget.setAdapter(adapter);
 
     }
 
@@ -39,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
      * This method populates the list view using a string resource containing the list of a data array
      */
     private void usingDataArray() {
-
-
+        ArrayAdapter<Pet> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, pets);
+        petListWidget.setAdapter(adapter);
     }
 }
--- app/src/main/java/model/Pet.java
+++ app/src/main/java/model/Pet.java
@@ -1,7 +1,24 @@
 package model;
 
+import java.sql.Array;
+
 public class Pet {
     String name;
     String breed;
     int age;
+
+    public String toString(){
+        return (name + "(" + breed "): " + age;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public String getBreed(){
+        return breed;
+    }
+    public int getAge(){
+        return age;
+    }
 }
+
